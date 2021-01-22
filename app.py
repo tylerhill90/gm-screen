77b290eb0973077ad1d02688efcaf53d0079c687
Tyler Hill <tylerhill90@gmail.com>
2021-01-22 09:06:08 -0800

    refactoer CS50 out in place of custom sqlite3 helper functions to access db

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from cs50 import SQL
+import sqlite3 as sql
+import os
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
 from passlib.apps import custom_app_context as pwd_context
@@ -48,8 +49,41 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///GMScreen.db?check_same_thread=True'
 
 
-# configure CS50 Library to use SQLite database
-db = SQL("sqlite:///GMscreen.db")
+def update_db(query, requests):
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "GMscreen.db")
+    try:
+        with sql.connect(db_path) as con:
+            cur = con.cursor()
+            cur.execute(query, requests)
+            con.commit()
+    except:
+        con.rollback()
+
+    finally:
+        con.close()
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+
+    return d
+
+
+def query_db(query, requests):
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "GMscreen.db")
+    con = sql.connect(db_path)
+    con.row_factory = dict_factory
+
+    cur = con.cursor()
+    cur.execute(query, requests)
+
+    rows = cur.fetchall()
+
+    return rows
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -73,8 +107,8 @@ def register():
         # if user input correct then store the user's info in the database after hashing their password
         else:
             hash = pwd_context.hash(request.form.get('password'))
-            result = db.execute("INSERT INTO users (username, hash) VALUES(:username, :hash)",
-                                username=request.form.get('username'), hash=hash)
+            result = query_db("INSERT INTO users (username, hash) VALUES(?, ?)",
+                                (request.form.get('username'), hash))
             if not result:
                 return apology('Failure to signup new user.')
             else:
@@ -96,15 +130,14 @@ def login():
     if request.method == "POST":
 
         # query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
-                          username=request.form.get("username"))
+        rows = query_db("SELECT * FROM users WHERE username = ?", (request.form.get("username"),))
 
         # ensure username exists and password is correct
         if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
             return apology("invalid username and/or password")
 
         # remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"]=rows[0]["id"]
 
         # redirect user to home page
         return redirect(url_for("account"))
@@ -120,23 +153,23 @@ def account():
 
     if request.method == "POST":
         if request.form.get('old_camp'):
-            session["camp_name"] = request.form['old_camp']
+            session["camp_name"]=request.form['old_camp']
 
             # query db for correct campaign_id and cache it
-            query = db.execute('SELECT * FROM campaigns WHERE user_id=:user_id AND campaign_name=:camp_name',
-                               user_id=session["user_id"], camp_name=session["camp_name"])
-            session["camp_id"] = query[0]['id']
+            query = query_db('SELECT * FROM campaigns WHERE user_id = ? AND campaign_name = ?',
+                               (session["user_id"], session["camp_name"]))
+            session["camp_id"]=query[0]['id']
 
             return redirect(url_for("campaign_editor"))
 
     # get username
-    query = db.execute(
-        'SELECT username FROM users WHERE id=:user_id', user_id=session["user_id"])
+    query = query_db(
+        'SELECT username FROM users WHERE id = ?', (session["user_id"],))
     username = query[0]['username']
 
     # get campaign names
-    query = db.execute(
-        'SELECT campaign_name FROM campaigns WHERE user_id=:user_id', user_id=session["user_id"])
+    query = query_db(
+        'SELECT campaign_name FROM campaigns WHERE user_id = ?', (session["user_id"],))
     campaigns = [query[x]['campaign_name'].strip(
         "'") for x in range(len(query))]
 
@@ -162,30 +195,30 @@ def campaign_editor():
     if request.method == "POST":
         if request.form.get('name'):
             # query db for correct campaign_id and cache it
-            query = db.execute('SELECT * FROM campaigns WHERE user_id=:user_id AND campaign_name=:camp_name',
-                               user_id=session["user_id"], camp_name=session["camp_name"])
-            session["camp_id"] = query[0]['id']
+            query = query_db('SELECT * FROM campaigns WHERE user_id = ? AND campaign_name = ?',
+                               (session["user_id"], session["camp_name"]))
+            session["camp_id"]=query[0]['id']
 
             # store new character infor in db
-            name, race, class_, spell_save, ac, hp = request.form['name'], request.form['race'], request.form[
+            name, race, class_, spell_save, ac, hp=request.form['name'], request.form['race'], request.form[
                 'class'], request.form['spell_save'], request.form['ac'], request.form['hp']
-            db.execute('INSERT INTO "characters" ("id","campaign_id","name","race","class", "spell_save", "ac","hp") VALUES (NULL,:camp_id,:name,:race,:class_,:spell_save,:ac,:hp)',
-                       camp_id=session["camp_id"], name=name, race=race, class_=class_, spell_save=spell_save, ac=ac, hp=hp)
+            update_db('INSERT INTO "characters" ("id","campaign_id","name","race","class", "spell_save", "ac","hp") VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)',
+                       (session["camp_id"], name, race, class_, spell_save, ac, hp))
 
             return redirect(url_for("campaign_editor"))
 
         # if user is editing a new campaign with a distinct name, display and store its name
-        query = db.execute(
-            'SELECT * FROM campaigns WHERE user_id=:user_id', user_id=session["user_id"])
+        query = query_db(
+            'SELECT * FROM campaigns WHERE user_id = ?', (session["user_id"],))
         if len(query) > 0 and request.form['new_camp'] == query[0]['campaign_name'].strip("'"):
             return apology('That campaign name already exists for this account. Please choose a distinct name.')
 
         elif request.form['new_camp']:
-            session["camp_name"] = request.form['new_camp']
-            db.execute('INSERT INTO "campaigns" ("id","user_id","campaign_name") VALUES (NULL,:user_id,:camp_name)',
-                       user_id=session["user_id"], camp_name=session["camp_name"])
-            session["camp_id"] = db.execute('SELECT id FROM campaigns WHERE user_id=:user_id AND campaign_name=:camp_name',
-                                            user_id=session["user_id"], camp_name=session["camp_name"])
+            session["camp_name"]=request.form['new_camp']
+            update_db('INSERT INTO "campaigns" ("id","user_id","campaign_name") VALUES (NULL, ?, ?)',
+                       (session["user_id"], session["camp_name"]))
+            session["camp_id"] = query_db('SELECT id FROM campaigns WHERE user_id = ? AND campaign_name = ?',
+                                            (session["user_id"], session["camp_name"]))
 
             return render_template("campaign_editor.html", camp_name=session["camp_name"].strip("'"), chars=[], camp_id=session["camp_id"])
 
@@ -195,13 +228,13 @@ def campaign_editor():
     # else if user reached route via GET (as by clicking a link or via redirect) and is editing an existing campaign
     else:
         # query db for existing characters in the appropriate campaign
-        chars = db.execute(
-            'SELECT id, name, race, class, spell_save, ac, hp FROM characters WHERE campaign_id=:camp_id', camp_id=session["camp_id"])
+        chars = query_db(
+            'SELECT id, name, race, class, spell_save, ac, hp FROM characters WHERE campaign_id = ?', (session["camp_id"],))
 
         for char in chars:
             for key, value in char.items():
                 if type(value) is str:
-                    char[key] = value.strip("'")
+                    char[key]=value.strip("'")
 
         return render_template("campaign_editor.html", camp_name=session["camp_name"].strip("'"), chars=chars, camp_id=session["camp_id"])
 
@@ -211,19 +244,19 @@ def play_campaign():
     """Play a campaign."""
 
     # cache campaign name
-    session["camp_name"] = request.form['play_camp']
+    session["camp_name"]=request.form['play_camp']
 
     # Query db for correct campaign_id and cache it
-    query = db.execute('SELECT * FROM campaigns WHERE user_id=:user_id AND campaign_name=:camp_name',
-                       user_id=session["user_id"], camp_name=session["camp_name"])
-    session["camp_id"] = query[0]['id']
+    query = query_db('SELECT * FROM campaigns WHERE user_id = ? AND campaign_name = ?',
+                       (session["user_id"], session["camp_name"]))
+    session["camp_id"]=query[0]['id']
 
     # Query db for existing characters in the appropriate campaign
-    chars = db.execute(
-        'SELECT name, race, class, spell_save, ac, hp FROM characters WHERE campaign_id=:camp_id', camp_id=session["camp_id"])
+    chars = query_db(
+        'SELECT name, race, class, spell_save, ac, hp FROM characters WHERE campaign_id = ?', (session["camp_id"],))
     for char in chars:
         for key, value in char.items():
-            char[key] = value.strip("'")
+            char[key]=value.strip("'")
 
     return render_template("play_campaign.html", camp_name=session["camp_name"].strip("'"), chars=chars)
 
@@ -234,20 +267,20 @@ def edit_char():
 
     if request.method == 'POST':
         if request.form.get('name'):
-            name, race, class_, spell_save, ac, hp, id_ = request.form['name'], request.form['race'], request.form[
+            name, race, class_, spell_save, ac, hp, id_=request.form['name'], request.form['race'], request.form[
                 'class'], request.form['spell_save'], request.form['ac'], request.form['hp'], request.form['id']
-            db.execute('UPDATE characters SET name=:name, race=:race, class=:class_, spell_save=:spell_save, ac=:ac, hp=:hp WHERE id=:id_',
-                       name=name, race=race, class_=class_, spell_save=spell_save, ac=ac, hp=hp, id_=id_)
+            update_db('UPDATE characters SET name = ?, race = ?, class = ?, spell_save = ?, ac = ?, hp = ? WHERE id = ?',
+                       (name, race, class_, spell_save, ac, hp, id_))
 
             return redirect(url_for("campaign_editor"))
 
         # query db for existing characters in the appropriate campaign
-        char = db.execute(
-            'SELECT id, name, race, class, spell_save, ac, hp FROM characters WHERE id=:id_', id_=request.form['edit_char'])
-        char = char[0]
+        char = query_db(
+            'SELECT id, name, race, class, spell_save, ac, hp FROM characters WHERE id = ?', (request.form['edit_char'],))
+        char=char[0]
         for key, value in char.items():
             if type(value) is str:
-                char[key] = value.strip("'")
+                char[key]=value.strip("'")
 
         return render_template("edit_char.html", char=char)
 
@@ -256,12 +289,12 @@ def edit_char():
 def delete_char():
     """Update an existing character."""
 
-    id_ = request.form['delete_char']
-    db.execute('DELETE FROM characters WHERE id=:id_', id_=id_)
+    id_=request.form['delete_char']
+    update_db('DELETE FROM characters WHERE id = ?', (id_,))
 
     return redirect(url_for("campaign_editor"))
 
 
 if __name__ == "__main__":
-    app.secret_key = b'\xabTA?\xe1\xb2\xa8\xd5\xeb\xfaJ{T)\x96~'
+    app.secret_key=b'\xabTA?\xe1\xb2\xa8\xd5\xeb\xfaJ{T)\x96~'
     app.run(debug=False)
